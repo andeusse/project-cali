6108fe60c67925098259373fcb7efd6dde7490d2
AndresEusse <andeusse@gmail.com>
2024-03-06 10:49:49 -0500

    Changed API endpoint

--- frontend/src/api/digitalTwinsModels.ts
+++ frontend/src/api/digitalTwinsModels.ts
@@ -5,5 +5,5 @@ export const updateModel = <T, R, S = AxiosResponse<R>>(
   model: string,
   body: T
 ): Promise<S> => {
-  return api.post(`/${model}`, body);
+  return api.post(`/models/${model}`, body);
 };
