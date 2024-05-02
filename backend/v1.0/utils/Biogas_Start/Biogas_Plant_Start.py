857825514789e76040710226cfa1e163812d40ea
sergioarango <sergio.arangogr@gmail.com>
2024-05-01 22:35:19 -0500

    Update Biogas Model _ Operation mode 2

--- backend/v1.0/utils/Biogas_Start/Biogas_Plant_Start.py
+++ backend/v1.0/utils/Biogas_Start/Biogas_Plant_Start.py
@@ -5,13 +5,13 @@ class BiogasStart:
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
           cls._instance = super().__new__(cls)
-          cls._instance._data = None
+          cls._instance._biogas = None
         return cls._instance
     def starting(self, VR1, VR2, VG1, VG2, VG3, tp, t_prediction, timetrain, Kini):
-        if self._data is None:
-          self._data = BiogasModel.BiogasPlant(VR1, VR2, VG1, VG2, VG3, tp, t_prediction, timetrain, Kini)
+        if self._biogas is None:
+          self._biogas = BiogasModel.BiogasPlant(VR1, VR2, VG1, VG2, VG3, tp, t_prediction, timetrain, Kini)
     @property
     def data(self):
-        if self._data is None:
-          raise ValueError("Database model not created. Please call createConnection method first.")
-        return self._data
+        if self._biogas is None:
+          raise ValueError("Biogas model not created. Please call createConnection method first.")
+        return self._biogas
