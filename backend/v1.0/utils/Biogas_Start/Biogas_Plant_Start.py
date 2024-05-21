9873ffa75cadc033a69a8ccbbab47cfa8a1d563f
sergioarango <sergio.arangogr@gmail.com>
2024-05-21 13:37:36 -0500

    Test Biogas

--- backend/v1.0/utils/Biogas_Start/Biogas_Plant_Start.py
+++ backend/v1.0/utils/Biogas_Start/Biogas_Plant_Start.py
@@ -5,11 +5,13 @@ class BiogasStart:
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
           cls._instance = super().__new__(cls)
-          cls._instance._biogas = None
+          cls._instance._data = None
         return cls._instance
-    def starting(self, VR1, VR2, VG1, VG2, VG3, tp, t_prediction, timetrain, Kini):
+    def starting(self, VR1, VR2, VG1, VG2, VG3, tp, ST_R101, SV_R101, Cc_R101, Ch_R101, Co_R101, Cn_R101, Cs_R101, rho_R101,
+                 ST_R102, SV_R102, Cc_R102, Ch_R102, Co_R102, Cn_R102, Cs_R102, rho_R102):
         if self._data is None:
-          self._data = Biogas_Model_DT.BiogasPlantDT(VR1, VR2, VG1, VG2, VG3, tp, t_prediction, timetrain, Kini)
+          self._data = Biogas_Model_DT.BiogasPlantDT(VR1, VR2, VG1, VG2, VG3, tp, ST_R101, SV_R101, Cc_R101, Ch_R101, Co_R101, Cn_R101, Cs_R101, rho_R101,
+                 ST_R102, SV_R102, Cc_R102, Ch_R102, Co_R102, Cn_R102, Cs_R102, rho_R102)
     @property
     def data(self):
         if self._data is None:
