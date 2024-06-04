2952a3bf03ec8febc542686b3b32ea5a15c57f2f
sergioarango <sergio.arangogr@gmail.com>
2024-06-03 23:56:03 -0500

    Update Start classes for biogas

--- backend/v1.0/utils/Biogas_Start/Biogas_Plant_Start.py
+++ backend/v1.0/utils/Biogas_Start/Biogas_Plant_Start.py
@@ -8,12 +8,16 @@ class BiogasStart:
           cls._instance._data = None
         return cls._instance
     def starting(self, VR1, VR2, VG1, VG2, VG3, tp, ST_R101, SV_R101, Cc_R101, Ch_R101, Co_R101, Cn_R101, Cs_R101, rho_R101,
-                 ST_R102, SV_R102, Cc_R102, Ch_R102, Co_R102, Cn_R102, Cs_R102, rho_R102):
+                 ST_R102, SV_R102, Cc_R102, Ch_R102, Co_R102, Cn_R102, Cs_R102, rho_R102, OperationMode):
         if self._data is None:
           self._data = Biogas_Model_DT.BiogasPlantDT(VR1, VR2, VG1, VG2, VG3, tp, ST_R101, SV_R101, Cc_R101, Ch_R101, Co_R101, Cn_R101, Cs_R101, rho_R101,
-                 ST_R102, SV_R102, Cc_R102, Ch_R102, Co_R102, Cn_R102, Cs_R102, rho_R102)
+                 ST_R102, SV_R102, Cc_R102, Ch_R102, Co_R102, Cn_R102, Cs_R102, rho_R102, OperationMode)
     @property
     def data(self):
         if self._data is None:
           raise ValueError("Biogas model not created. Please call createConnection method first.")
         return self._data
+    
+    @staticmethod
+    def reset_instance():
+        BiogasStart._instance = None
--- backend/v1.0/utils/MachineLearning_biogas_start/LearningStart.py
+++ backend/v1.0/utils/MachineLearning_biogas_start/LearningStart.py
@@ -7,11 +7,15 @@ class MachineLearningStart:
           cls._instance = super().__new__(cls)
           cls._instance._data = None
         return cls._instance
-    def starting(self, VR1, Kini, Eaini, DatabaseConnection_df, database_df, Influx):
+    def starting(self, t_train, OperationMode, Kini, Eaini, VR1):
         if self._data is None:
-          self._data = MachineLearningModule.BiogasModelTrain(VR1, Kini, Eaini, DatabaseConnection_df, database_df, Influx)
+          self._data = MachineLearningModule.BiogasModelTrain(t_train, OperationMode, Kini, Eaini, VR1)
     @property
     def data(self):
         if self._data is None:
           raise ValueError("Learning model not created. Please call createConnection method first.")
         return self._data
+    
+    @staticmethod
+    def reset_instance():
+        MachineLearningStart._instance = None
