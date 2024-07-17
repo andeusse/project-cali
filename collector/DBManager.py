72dcc5193ef9c2821b96c4c277c2ef10639b5d35
DanB-90 <74477898+DanB-90@users.noreply.github.com>
2024-07-16 19:20:31 -0500

    Update DBManager.py

--- collector/DBManager.py
+++ collector/DBManager.py
@@ -1,22 +1,27 @@
 import influxdb_client
 from influxdb_client import InfluxDBClient
-from influxdb_client.client.write_api import SYNCHRONOUS
+import warnings
+from influxdb_client.client.warnings import MissingPivotFunction
+from influxdb_client.client.write_api import SYNCHRONOUS, WritePrecision
+
 
 # %%  InfluxDB Model        
 class InfluxDBmodel:
     def __init__(self, server, org, bucket, token):
+        self.ERROR_MESSAGE = 'Ocurrió un error al conectarse con la base de datos de InfluxDB, verifique que la base de datos exista y que los parámetros de conexión estén bien configurados.'
         self.server = server
         self.org = org
         self.token = token
         self.bucket = bucket
+        warnings.simplefilter("ignore", MissingPivotFunction)
 
     # %%  InfluxDB Connection         
     def InfluxDBconnection(self):
         try:
             self.influxDBclient = InfluxDBClient(url = self.server, token = self.token)
-            return 'Succesfully connected to the InfluxDB Database'
+            return self.influxDBclient.ping()
         except:
-            return 'An error ocurred connecting to the InfluxDB Database'    
+            return False
 
     # %%  InfluxDB Reader 
     def InfluxDBreader(self, query):
@@ -25,16 +30,12 @@ class InfluxDBmodel:
             self.influxReturndf = self.influxDBclient.query_api().query_data_frame(self.query, self.org)
             return self.influxReturndf
         except:
-            return 'An error ocurred reading the InfluxDB Database'
+            return self.ERROR_MESSAGE
 
     # %%  InfluxDB Writer         
-    def InfluxDBwriter(self, load, variable, value, timestamp):
+    def InfluxDBwriter(self, measurement, device, variable, value, timestamp):
         write_api = self.influxDBclient.write_api(write_options=SYNCHRONOUS)
-        payload = influxdb_client.Point(load).field(variable, value)
-        # payload = {"measurement": analyzer,
-        #       "tags":{"_load": load},
-        #       "fields": {variable: value},
-        #      "time": timestamp}
+        payload = influxdb_client.Point(measurement).tag('device',device).field(variable, value).time(timestamp, write_precision=WritePrecision.S)
         
         try:
             write_api.write(self.bucket, self.org, payload)
@@ -42,35 +43,35 @@ class InfluxDBmodel:
         except:
             return 'An error ocurred writing the InfluxDB Database'
 
+    # %% InfluxDB close connection
+    def InfluxDBclose(self):
+        self.influxDBclient.close()
+
     # %% InfluxDB query creator
-    def QueryCreator(self, device, variable, location, type, forecastTime): #type 0: electrical last value, type 1: weather last value, type 2: electrical and weather forecast, type 3: next day forecast
+    def QueryCreator(self, measurement='', device='', variable='', location='', type=0, forecastTime=0): #type 0: electrical last value, type 1: weather last value, type 2: electrical and weather forecast, type 3: next day forecast
         if type == 0:
             self.query = '''from(bucket: "''' + self.bucket + '''")
-            |> range(start: -180m)
-            |> filter(fn: (r) => r["_measurement"] == "''' + device + '''")
+            |> range(start: -1m)
+            |> filter(fn: (r) => r["_measurement"] == "''' + measurement + '''")
+            |> filter(fn: (r) => r["device"] == "''' + device + '''")
             |> filter(fn: (r) => r["_field"] == "''' + variable + '''")
-            |> last()''' #-10m
+            |> last() |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'''
         elif type == 1:
             self.query ='''from(bucket: "''' + self.bucket + '''")
-            |> range(start: -60m, stop: now()) 
-            |> filter(fn: (r) => r["_measurement"] == "weather_stations")
-            |> filter(fn: (r) => r["_field"] == "''' + device + '''")
-            |> filter(fn: (r) => r["location"] == "''' + location + '''")
+            |> range(start: -1m)
+            |> filter(fn: (r) => r["_measurement"] == "''' + measurement + '''")
             |> last()'''
         elif type == 2:
-            self.query ='''import "experimental"\nfrom(bucket: "''' + self.bucket + '''")
-            |> range(start: -60m, stop: experimental.addDuration(d: ''' + str(forecastTime) + '''s, to: now()))
-            |> filter(fn: (r) => r["_measurement"] == "forecast")
-            |> filter(fn: (r) => r["_field"] == "''' + device + '''")
-            |> filter(fn: (r) => r["location"] == "''' + location + '''")
-            |> filter(fn: (r) => r["period"] == "0")
-            |> last()'''
+            self.query ='''from(bucket: "''' + self.bucket + '''")
+            |> range(start: -120m, stop: now()) 
+            |> filter(fn: (r) => r["_measurement"] == "''' + measurement + '''")
+            '''
         elif type == 3:
             self.query ='''import "experimental"
             from(bucket: "''' + self.bucket + '''")
             |> range(start: ''' + forecastTime + ''', stop: experimental.addDuration(d: 24h, to: ''' + forecastTime + '''))
             |> filter(fn: (r) => r["_measurement"] == "forecast")
-            |> filter(fn: (r) => r["_field"] == "''' + device + '''")
+            |> filter(fn: (r) => r["_field"] == "''' + measurement + '''")
             |> filter(fn: (r) => r["location"] == "''' + location + '''")
             |> filter(fn: (r) => r["period"] == "0")'''
         else:
