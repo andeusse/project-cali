a07cd2dddf7c8a6fc13643df6c2c3c84b6f0c021
AndresEusse <andeusse@gmail.com>
2024-09-05 16:57:04 -0500

    Set timeout in 5 seconds

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -82,7 +82,7 @@ export default class Config {
     };
 
     axios
-      .get(this.params.apiUrl)
+      .get(this.params.apiUrl, { timeout: 5000 })
       .then(() => {
         console.log(this.params.apiUrl);
       })
