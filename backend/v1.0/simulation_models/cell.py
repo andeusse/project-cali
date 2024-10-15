f5860c920e592146d51ae7703f0e1621c88117c3
eros-dec <78046650+eros-dec@users.noreply.github.com>
2024-10-15 18:54:22 -0500

    Cell voltage model adjusted

--- backend/v1.0/simulation_models/cell.py
+++ backend/v1.0/simulation_models/cell.py
@@ -39,28 +39,19 @@ class TwinCell:
 
         self.cellCurrent = self.electronicLoadCurrent + ((cellSelfFeedingPower + lightsPower) / self.n_converter) / previousCellVoltage
         
-        # if inputCellTemperature >= 41.0:
-        #     voltageDrop = 0.07*self.cellCurrent**2 - 0.03*self.cellCurrent + 0.01
-        # elif inputCellTemperature >= 37.0:
-        #     voltageDrop = 0.05*self.cellCurrent**2 - 0.01*self.cellCurrent + 0.04
-        # else:
-        #     voltageDrop = 0.16331836*self.cellCurrent - 0.05462165
-        
-        # self.cellVoltage = 0.0128870679*self.cellCurrent**6 - 0.220519195*self.cellCurrent**5 + 1.48692377*self.cellCurrent**4 - 5.03979764*self.cellCurrent**3 + 9.11309881*self.cellCurrent**2 - 10.1063984*self.cellCurrent + 16.650003 - voltageDrop*(inputCellTemperature - 32.0)
-        if inputFanPercentage < 100:
-            self.cellVoltage = 16.9 - 13.2672427*self.cellCurrent + 13.5756291*self.cellCurrent**2 - 6.66323789*self.cellCurrent**3 + 1.21415288*self.cellCurrent**4 - 0.0005*self.cellCurrent**5 - 0.016*self.cellCurrent**6 - 0.0111392658*inputFanPercentage + 0.00015*inputFanPercentage**2
+        if inputFanPercentage < 90:
+            self.cellVoltage = 16.9 - 13.2672427*self.cellCurrent + 13.5756291*self.cellCurrent**2 - 6.66323789*self.cellCurrent**3 + 1.21415288*self.cellCurrent**4 - 0.0005*self.cellCurrent**5 - 0.0155*self.cellCurrent**6 - 0.0111392658*inputFanPercentage + 0.00015*inputFanPercentage**2
         else:
-            self.cellVoltage = 16.8 - 12.55529151*self.cellCurrent + 13.2196722*self.cellCurrent**2 - 7.84413473*self.cellCurrent**3 + 2.40358984*self.cellCurrent**4 - 0.36143254*self.cellCurrent**5 - 0.02101814*self.cellCurrent**6
+            self.cellVoltage = 16.9 - 12.55529151*self.cellCurrent + 13.2196722*self.cellCurrent**2 - 7.84413473*self.cellCurrent**3 + 2.40358984*self.cellCurrent**4 - 0.3615*self.cellCurrent**5 - 0.02101814*self.cellCurrent**6 - 0.1*inputFanPercentage + 0.0001*inputFanPercentage**2
         
-        if self.cellVoltage <= 5.8:
+        if self.cellVoltage <= 6.0:
             self.cellCurrent = 0.0
-            self.cellVoltage = 5.8
+            self.cellVoltage = 6.0
             self.electronicLoadCurrent = 0.0
             self.cellSelfFeedingPower = 0.0
             self.lightsPower = 0.0
         self.cellPower = self.cellCurrent * self.cellVoltage
         self.hydrogenFlow = 0.63173435*self.cellCurrent**5 - 4.45717449*self.cellCurrent**4 + 8.93813509*self.cellCurrent**3 - 1.38222392*self.cellCurrent**2 + 145.33329002*self.cellCurrent + 11.05591085
-        # standardHydrogenFlow = (273.15 / 101325) * ((101325 + 34473.8) * self.hydrogenFlow) / (inputCellTemperature + 273.15)
         standardHydrogenFlow = (273.15 / 101325) * ((101325 + 34473.8) * self.hydrogenFlow) / (40.0 + 273.15)
         self.cellEfficiency = 100 * ((self.cellPower * 60) / (10.8 * standardHydrogenFlow))
 
