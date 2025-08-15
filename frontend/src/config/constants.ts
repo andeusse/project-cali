d32bd8c7ce3d3777b4d98191e0f7d20cc4fdf5b4
sergioarango <69864596+sergioarango@users.noreply.github.com>
2025-08-15 17:33:35 -0500

    update front query time BMP

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -13,7 +13,7 @@ export default class Constants {
   public static QUERY_TIME_DIGITAL_TWIN_ON_ONLINE_BIOGAS: number = 600000;
 
   public static QUERY_TIME_OFFLINE_BMP: number = 3000;
-  public static QUERY_TIME_ONLINE_BMP: number = 600000;
+  public static QUERY_TIME_ONLINE_BMP: number = 400000;
 
   public static MAX_RETRIES_BEFORE_STOP: number = 5;
 
