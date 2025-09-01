9a7373d9232b36b8d7a89b0a58aa55f0f97cb7e6
sergioarango <69864596+sergioarango@users.noreply.github.com>
2025-09-01 16:09:26 -0500

    Update BMP

--- backend/v1.0/simulation_models/BMPModel/BMPOnlineTrainMode.py
+++ backend/v1.0/simulation_models/BMPModel/BMPOnlineTrainMode.py
@@ -1918,6 +1918,7 @@ class BMP_online:
         SV_ini = []
         Csus_mol = []
         Q_inv = []
+
         if 'ST_int' not in ReactorData.columns:
             self.ST_ini = self.ST_ini
         else:
@@ -2200,7 +2201,7 @@ class BMP_online:
                 self.resolution = 200
             elif Model == 'ADM1':
                 self.K_ini = 1
-                self.resolution = 10000
+                self.resolution = 500
             elif Model == 'Gompertz':
                 self.ym_ini = 1
                 self.U_ini = 1
